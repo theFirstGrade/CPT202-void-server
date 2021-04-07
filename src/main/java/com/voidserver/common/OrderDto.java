04476b49eb3e2a95d5368e2609763d97be4a35b0
theFirstGrade <ZHENHAO.CHEN18@student.xjtlu.edu.cn>
2021-04-08 02:08:02 +0800

    邮件接口

--- src/main/java/com/voidserver/common/OrderDto.java
+++ src/main/java/com/voidserver/common/OrderDto.java
@@ -20,5 +20,4 @@ public class OrderDto implements Serializable {
 
     private String productName;
 
-
 }
--- src/main/java/com/voidserver/controller/ApplicationController.java
+++ src/main/java/com/voidserver/controller/ApplicationController.java
@@ -10,14 +10,20 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.voidserver.common.OrderDto;
 import com.voidserver.common.Result;
+import com.voidserver.common.UserDto;
 import com.voidserver.entity.Application;
 import com.voidserver.mapper.ApplicationMapper;
 import com.voidserver.mapper.UserMapper;
 import com.voidserver.service.ApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.*;
@@ -41,6 +47,26 @@ public class ApplicationController {
     @Resource
     ApplicationMapper applicationMapper;
 
+    @Resource
+    private JavaMailSenderImpl javaMailSenderImpl;
+
+
+    @GetMapping("/sendEmail")
+    public void sendSimpleMail() {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        // 设置邮件主题
+        simpleMailMessage.setSubject("账号激活");
+        // 设置要发送的邮件内容
+        simpleMailMessage.setText("hello!");
+        // 要发送的目标邮箱
+        simpleMailMessage.setTo("ZHENHAO.CHEN18@student.xjtlu.edu.cn");
+        // 发送者邮箱和配置文件中的邮箱一致
+//        simpleMailMessage.setFrom("136853349@qq.com");
+        simpleMailMessage.setFrom("XJTLU-VOID" + '<' + "136853349@qq.com" + '>');
+        javaMailSenderImpl.send(simpleMailMessage);
+    }
+
+
     @CrossOrigin
     @PostMapping("/application")
     public Result makeApplication(@RequestBody String str) {
@@ -49,11 +75,15 @@ public class ApplicationController {
         Application application;
 
         Integer userId = Integer.parseInt(JSON.parseObject(str).get("userId").toString());
+        String email = JSON.parseObject(str).get("email").toString();
+
         // {"水性笔（黑）1":{"number":4,"unit":"支","address":"新兴科学楼（南校区）","productId":2,"director":"Sterling"},"笔记本":{"number":2,"unit":"本","address":"理科楼（北校区）","productId":1,"director":"Erick"}}
         //[{"number":2,"unit":"本","address":"理科楼（北校区）","productId":1,"director":"Erick","name":"笔记本"},{"number":4,"unit":"支","address":"新兴科学楼（南校区）","productId":2,"director":"Sterling","name":"水性笔（黑）1"}]
 
         List<OrderDto> data = JSON.parseArray(JSON.parseObject(str).getString("data"), OrderDto.class);
         List<Application> applications = new ArrayList<>();
+//        List<OrderDto> orderDtoList = new ArrayList<>();
+        StringBuilder orderMessage = new StringBuilder();
         for (OrderDto item : data) {
 //            System.out.println(item.getProductId() + "---" + item.getProductName() + "---" + item.getNumber());
             application = new Application();
@@ -69,12 +99,44 @@ public class ApplicationController {
             application.setDeleted(0);
             applications.add(application);
             System.out.println(application);
+            orderMessage.append("{").append(item.getProductName()).append("    ").append(item.getNumber()).append(item.getUnit()).append("    ").append(item.getAddress()).append("}     ");
         }
-
+        System.out.println(orderMessage);
         boolean affectRows = applicationService.saveBatch(applications);
         System.out.println(affectRows);
 
         if (affectRows) {
+
+            MimeMessage mimeMessage = javaMailSenderImpl.createMimeMessage();
+            try {
+                // 开启文件上传
+                MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+                // 设置文件主题
+                mimeMessageHelper.setSubject("您的物资领取验证码");
+                // 设置文件内容 第二个参数设置是否支持html
+                mimeMessageHelper.setText("<b style='color:red'>您的订单：</b><br/>" + orderMessage + "<br/>" + "<b style='color:red'>验证码：</b>" + verifyCode, true);
+                // 设置发送到的邮箱
+                mimeMessageHelper.setTo(email);
+                // 设置发送人和配置文件中邮箱一致
+                mimeMessageHelper.setFrom("XJTLU-VOID" + '<' + "136853349@qq.com" + '>');
+                // 上传附件
+                // mimeMessageHelper.addAttachment("", new File(""));
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+            javaMailSenderImpl.send(mimeMessage);
+
+
+//            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+//            // 设置邮件主题
+//            simpleMailMessage.setSubject("您的物资领取验证码");
+//            // 设置要发送的邮件内容
+//            simpleMailMessage.setText("<b style='color:red'>您的订单：</b>" + "验证码：" + verifyCode);
+//            // 要发送的目标邮箱
+//            simpleMailMessage.setTo(email);
+//            // 发送者邮箱和配置文件中的邮箱一致
+//            simpleMailMessage.setFrom("XJTLU-VOID" + '<' + "136853349@qq.com" + '>');
+//            javaMailSenderImpl.send(simpleMailMessage);
             return Result.succ("成功");
         } else {
             return Result.fail("失败");
